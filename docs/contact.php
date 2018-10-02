948f07943ea0301f9b432fbbbc1d9b44cd39b32b
samsdial <diastudios@gmail.com>
2018-10-02 09:38:47 -0500

    All Entrega Final one

---
+++ docs/contact.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SMARTINEZ
+ * Date: 2/10/2018
+ * Time: 8:50 AM
+ */
+	$response = array();
+	$name = (isset($_REQUEST['name'])) ? filter_var($_REQUEST['name'], FILTER_SANITIZE_STRING) : null ;
+	$lastName = (isset($_REQUEST['lastName'])) ? filter_var($_REQUEST['lastName'], FILTER_SANITIZE_STRING) : null ;
+	$email = (isset($_REQUEST['email'])) ? filter_var($_REQUEST['email'], FILTER_SANITIZE_STRING) : null ;
+	$phone =  (isset($_REQUEST['phone'])) ? filter_var($_REQUEST['phone'], FILTER_SANITIZE_STRING) : null ;
+	$city = (isset($_REQUEST['city'])) ? filter_var($_REQUEST['city'], FILTER_SANITIZE_STRING) : null ;
+
+if ($name!=null && $lastName!=null && $email!=null && $phone!=null && $city!=null) {
+
+
+    $msg="<table><thead><th colspan='2'>Un nuevo mensaje ha sido enviado desde la Campaña web de Yamaki Nueva serie QSC:</th></thead>
+	<tbody>
+	<tr><td><strong>Nombre: </strong></td><td>".$name."</td></tr>
+	<tr><td><strong>Apellido: </strong></td><td>".$lastName."</td></tr>
+	<tr><td><strong>E-Mail: </strong></td><td>".$email."</td></tr>
+	<tr><td><strong>Celular: </strong></td><td>".$phone."</td></tr>
+	<tr><td><strong>Desde la Ciudad: </strong></td><td>".$city."</td></tr>
+	</tbody>
+	</table>
+	";
+    $title = "Nuevo mensaje de contacto en YAMAKI QSC";
+    $to = 'dialstudios@gmial.com';
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    $headers .= 'To: '.$to.'' . "\r\n";
+    $headers .= 'From: Servicio Al Cliente <mercadeo@yamaki.com>' . "\r\n";
+
+    if(mail( $to, $title, $msg, $headers)){
+        $response['message'] = "exito";
+        $response['success'] = true;
+    }
+    else{
+        $response['message'] = "error";
+        $response['success'] = false;
+    }
+}
+echo json_encode($response);
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,1 +1,76 @@
+$(document).ready(function($){
+    $('#submit').click(function(){
+        var name        = $("#name").val();
+        var lastName    = $("#lastName").val();
+        var email       = $("#email").val();
+        var validacion_email = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+        var phone       = $("#phone").val();
+        var city       = $("#city").val();
+
+        if(name == "" || lastName == "" || email == "" || phone == "" || city == ""){
+            if(name == ""){
+                $(".name + .error").addClass("i");
+                $("#name").focus();
+                return false;
+            }else{
+                $(".name + .error").removeClass("i");
+            }
+            if(lastName == ""){
+                $(".lastname + .error").addClass("i");
+                $("#lastName").focus();
+                return false;
+            }else{
+                $(".lastname + .error").removeClass("i");
+            }
+            if(email == "" || !validacion_email.test(email)){
+                $(".email + .error").addClass("i");
+                $("#email").focus();
+                return false;
+            }else{
+                $(".email + .error").removeClass("i");
+            }
+            if(phone == ""){
+                $(".cel + .error").addClass("i");
+                $("#phone").focus();
+                return false;
+            }else{
+                $(".cel + .error").removeClass("i");
+            }
+            if(city == ""){
+                $(".city + .error").addClass("i");
+                $("#city").focus();
+                return false;
+            }else{
+                $(".city + .error").removeClass("i");
+            }
+        }else{
+            $(".city + .error").removeClass("i");
+            var datos = '&name=' + name +
+                '&lastName=' + lastName +
+                '&email=' + email +
+                '&phone=' + phone +
+                '&city=' + city;
+            $.ajax({
+                method:"Post",
+                url:"contact.php",
+                dataType:"json",
+                data:datos,
+            }).done(function( msg ){
+                if(msg.success){
+                    $('.alerta p').fadeIn("slow");
+                    $('.alerta p').html('Muchas Gracias por contáctarnos, pronto nos estaremos comunicando contigo.');
+                    $('#name').val("");
+                    $('#lastNamee').val("");
+                    $('#email').val("");
+                    $('#phone').val("");
+                    $('#city').val("");
+                }else{
+                    $('.alerta p').fadeIn("slow");
+                    $('.alerta p').html('Error, intente más tarde por favor.');
+                }
+            });
+            return false;
+        }
+    });
+});
 
